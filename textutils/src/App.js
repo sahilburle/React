0ee25e1d88ca34e74a2a4d4cf983a91277f99158
sahilburle <sahilburle1998@gmail.com>
2022-08-18 08:56:45 +0530

    route

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,31 +2,46 @@ import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Link, Route, Router } from "react-router-dom";
 
 function App() {
-
-  const [mode, setMode] = useState('light');    //dark mode enable hai ya nhi
+  const [mode, setMode] = useState("light"); //dark mode enable hai ya nhi
 
   const toggleMode = () => {
-    if(mode == 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+    if (mode == "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-  }
+  };
 
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <div className="container my-3">
-      <TextForm heading="Type here"/>
-      {/* <About/> */}
-      </div>
+      <Router>
+        <div>
+          
+          <switch>
+          <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+          <div className="container my-3">
+            <TextForm heading="Type here" />
+            {/* <About/> */}
+          </div>
+            <Route path="/about">
+              <About />
+            </Route>
+            {/* <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route> */}
+          </switch>
+        </div>
+      </Router>
     </>
   );
 }
