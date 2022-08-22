91473482722a163d2d09ac0081f2ff5aa505a5b3
sahilburle <sahilburle1998@gmail.com>
2022-08-22 15:33:58 +0530

    newsapp

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div className='contaier my-3'>
+      <div className="contaier mx-3 my-3">
         <h2>NewsMonkey - Top HeadLines</h2>
-        <div className='row'>
-        <div className="col-md-3">
-        <NewsItem title="myTitle" description="mydesc"/>
-        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" imageUrl="https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk="/>
+          </div>
+
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+
+          <div className="col-md-3">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
