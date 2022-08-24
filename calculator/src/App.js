b5457d42ea5ae65c17563e46734eadcd4a13221c
sahilburle <sahilburle1998@gmail.com>
2022-08-24 10:34:14 +0530

    calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,15 +1,13 @@
-import './styles.css';
+import "./styles.css";
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">
-          <div className="current-operand">
-            
-          </div>
-        </div>
-        <button className='span-two'>AC</button>
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+
+        <button className="span-two">AC</button>
         <button>DEL</button>
         <button>÷</button>
         <button>1</button>
@@ -26,7 +24,8 @@ function App() {
         <button>-</button>
         <button>.</button>
         <button>0</button>
-        <button className='span-two'>=</button>
+        <button className="span-two">=</button>
+        
       </div>
     </div>
   );
