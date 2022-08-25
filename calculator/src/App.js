fa8f003da2f1415c78be6029c023f610f725aaad
sahilburle <sahilburle1998@gmail.com>
2022-08-25 11:25:20 +0530

    calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,15 +1,31 @@
 import Wrapper from "./Components/Wrapper";
 import Screen from "./Components/Screen";
+import ButtonBox from "./Components/ButtonBox";
+import Button from "./Components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "*"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="]
+];
 
 function App() {
   return (
 
     <div>
-      <Wrapper/>
+      <Wrapper>
       <Screen/>
-
-      Hello
-      
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => (
+          <Button
+              value={btn}
+              key={i}
+          />
+        ))}
+      </ButtonBox>
+      </Wrapper>
     </div>
     );
 }
